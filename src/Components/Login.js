cc888eff35125aec9eac50ab151401a0aaa361c9
infolanze807 <techteam.infolanze@gmail.com>
2024-07-10 18:14:43 +0530

    password access

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,89 @@
-import React ,{useState}  from 'react'
-import { PiEyeFill,PiEyeSlashFill  } from "react-icons/pi";
+// import React ,{useState}  from 'react'
+// import { PiEyeFill,PiEyeSlashFill  } from "react-icons/pi";
+// import { useFormik } from "formik";
+// import * as Yup from "yup";
+// import { useNavigate } from 'react-router-dom';
+// import logo from '../Images/gno-wallet.png'
+
+// function Login() {
+//   const [passwordVisible, setPasswordVisible] = useState(false);
+//   const navigate = useNavigate();
+  
+//   const formik = useFormik({
+//     initialValues: {
+//       password: "",
+//     },
+//     validationSchema: Yup.object({
+//       password: Yup.string()
+//         .required("Required"),
+//     }),
+//     onSubmit: (values) => {
+//         navigate('/success');
+//       },
+//   });
+//   const togglePasswordVisibility = () => {
+//     setPasswordVisible((prevState) => !prevState);
+//   };
+
+//   return (
+//     <>
+//     <div className=" text-white flex flex-col items-center h-[100vh] md:h-[95vh] lg:h-[80vh] xl:h-[100vh] justify-center">
+//         <img
+//           src={logo}
+//           className="mb-5 h-[100px] w-[90px] shadow-custom"
+//         />
+//         <div className="flex flex-col items-center justify-center bg-[--bg-color] border-[--border-color] border shadow-sm p-4 rounded-2xl w-[300px] sm:w-[320px] md:w-[350px] lg:w-[400px] xl:w-[380px] ">
+        
+//             <div className='text-center w-full '>
+//                 <p className='text-lg'>Secure and trusted multi-chain crypto wallet</p>
+//             </div>
+//             <form onSubmit={formik.handleSubmit} className="w-full">
+//             <div className="w-full relative mt-4">
+//               {passwordVisible ? (
+//                 <PiEyeFill onClick={togglePasswordVisibility} className="absolute top-9 right-2 text-[--green-color] font-semibold" />
+//               ) : (
+//                 <PiEyeSlashFill onClick={togglePasswordVisibility} className="absolute top-9 right-2 text-[--green-color] font-semibold" />
+//               )}
+//               <label className="text-base">Password</label>
+//               <input
+//                 type={passwordVisible ? "text" : "password"}
+//                 className="bg-[--border-color] w-full h-[40px] px-2 rounded-sm b focus:outline-[--green-color] outline-none"
+//                 id="password"
+//                 onChange={formik.handleChange}
+//                 onBlur={formik.handleBlur}
+//                 value={formik.values.password}
+//               />
+//               {formik.touched.password && formik.errors.password ? (
+//                 <div className="text-red-500">{formik.errors.password}</div>
+//               ) : null}
+//             </div>
+//             <div className='w-full my-4'>
+//             <button className="bg-[--green-color] text-white text-base w-full rounded-full p-2 mt-4" >
+//                    Unblock
+//               </button>
+//             </div>
+//             </form>
+//               </div>
+//         </div>
+        
+//     </>
+//   )
+// }
+
+// export default Login
+
+import React ,{useState}  from 'react';
+import { FiEyeOff, FiEye } from "react-icons/fi";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from 'react-router-dom';
 import logo from '../Images/gno-wallet.png'
+import axios  from 'axios';
 
 function Login() {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const navigate = useNavigate();
+  const [error, setError] = useState(null);
   
   const formik = useFormik({
     initialValues: {
@@ -17,10 +93,18 @@ function Login() {
       password: Yup.string()
         .required("Required"),
     }),
-    onSubmit: (values) => {
-        navigate('/success');
-      },
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.get(`https://gno-ten.vercel.app/wallet/${values.password}`);
+        if (response.data) {
+          navigate("/main");
+        }
+      } catch (error) {
+        setError("Invalid password. Please try again.");
+      }
+    },
   });
+
   const togglePasswordVisibility = () => {
     setPasswordVisible((prevState) => !prevState);
   };
@@ -32,7 +116,7 @@ function Login() {
           src={logo}
           className="mb-5 h-[100px] w-[90px] shadow-custom"
         />
-        <div className="flex flex-col items-center justify-center bg-[--bg-color] border-[--border-color] border shadow-sm p-4 rounded-2xl w-[300px] sm:w-[320px] md:w-[350px] lg:w-[400px] xl:w-[380px] ">
+        <div className="flex flex-col items-center justify-center bg-[--bg-color] border-[--border-color] border shadow-sm p-10 rounded-2xl w-[300px]  sm:w-[320px] md:w-[350px] lg:w-[400px] xl:w-[450px] ">
         
             <div className='text-center w-full '>
                 <p className='text-lg'>Secure and trusted multi-chain crypto wallet</p>
@@ -40,14 +124,14 @@ function Login() {
             <form onSubmit={formik.handleSubmit} className="w-full">
             <div className="w-full relative mt-4">
               {passwordVisible ? (
-                <PiEyeFill onClick={togglePasswordVisibility} className="absolute top-9 right-2 text-[--green-color] font-semibold" />
+                <FiEye onClick={togglePasswordVisibility} className="absolute top-9 right-2" />
               ) : (
-                <PiEyeSlashFill onClick={togglePasswordVisibility} className="absolute top-9 right-2 text-[--green-color] font-semibold" />
+                <FiEyeOff onClick={togglePasswordVisibility} className="absolute top-9 right-2" />
               )}
               <label className="text-base">Password</label>
               <input
                 type={passwordVisible ? "text" : "password"}
-                className="bg-[--border-color] w-full h-[40px] px-2 rounded-sm b focus:outline-[--green-color] outline-none"
+                className="bg-[--border-color] w-full h-[40px] px-2 focus:outline-[--green-color] outline-none"
                 id="password"
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
@@ -61,6 +145,7 @@ function Login() {
             <button className="bg-[--green-color] text-white text-base w-full rounded-full p-2 mt-4" >
                    Unblock
               </button>
+              {error && <p style={{ color: "red" }}>{error}</p>}
             </div>
             </form>
               </div>
