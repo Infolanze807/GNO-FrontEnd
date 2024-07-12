7a0776ddbd833ab3e39e8607e65d44651f35a731
infolanze807 <techteam.infolanze@gmail.com>
2024-07-12 16:53:37 +0530

    update reponsive

--- src/Utils/Rpc.js
+++ src/Utils/Rpc.js
@@ -1,12 +1,12 @@
 // src/utils/rpc.js
-import { ethers } from 'ethers';
+// import { ethers } from 'ethers';
 import Web3 from 'web3';
 
 const GNOSIS_RPC_URL = 'https://rpc.gnosischain.com/';
 
-export const getProvider = () => {
-  return new ethers.providers.JsonRpcProvider(GNOSIS_RPC_URL);
-};
+// export const getProvider = () => {
+//   return new ethers.providers.JsonRpcProvider(GNOSIS_RPC_URL);
+// };
 
 export const getWeb3 = () => {
   return new Web3(new Web3.providers.HttpProvider(GNOSIS_RPC_URL));  
