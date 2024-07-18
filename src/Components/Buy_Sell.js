e7d91aab42d05b193d7eb44af0218ef147bc6bc2
Kush  Mevada <102648785+Kush2343@users.noreply.github.com>
2024-07-18 19:14:48 +0530

    Create Buy_Sell.js

---
+++ src/Components/Buy_Sell.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Buy_Sell = () => {
+  return (
+    <div classname="text-2xl text-white mt-10">
+      <h1>Coming Soon</h1>
+    </div>
+  );
+};
+export default Buy_Sell;
