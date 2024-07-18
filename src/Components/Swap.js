2076e1038f2058553cde55d44d276ac242d0230d
Kush  Mevada <102648785+Kush2343@users.noreply.github.com>
2024-07-18 19:13:12 +0530

    Create Swap.js

---
+++ src/Components/Swap.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Swap = () => {
+  return (
+    <div classname="text-2xl text-white mt-10">
+      <h1>Coming Soon</h1>
+    </div>
+  );
+};
+export default Swap;
