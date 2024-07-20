a61d329e0c118ec4116aa31a806f566432952c9a
infolanze807 <techteam.infolanze@gmail.com>
2024-07-20 12:34:31 +0530

    responsive isuues and lodder add

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -20,7 +20,7 @@ function Welcome() {
   };
   return (
     <>
-      <div className=" text-white flex flex-col items-center h-[100vh] md:h-[90vh] lg:h-[90vh] xl:h-[90vh] justify-center">
+      <div className=" text-white flex flex-col items-center h-[80vh] md:h-[90vh] lg:h-[90vh] xl:h-[90vh] justify-center">
         <img src={logo} className="mb-5 h-[90px] w-[100px] shadow-custom" />
         <div className="flex flex-col items-center justify-center bg-[--bg-color] border-[--border-color] border shadow-sm rounded-2xl p-4 w-[300px] sm:w-[320px] md:w-[350px] lg:w-[350px] xl:w-[350px] ">
           <div className="text-center">
